35394c28aa1499774066a09fc84995bbd4dd77cd
Sanad-Manga <ahmed.sanad@student.giu-uni.de>
2026-04-18 22:52:44 +0200

    completed first half of job post -> Sanad

---
+++ backend/models/JobPost.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+        trime: true
+    },
+
+    company: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    requirements: {
+        type: [String],
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: trim
+    },
+
+    type: {
+        type: String,
+        enum: ["full-time", "part-time", "internships"],
+        required: true
+    },
+
+   
+
+
+
+    createdBy :{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+
+
+});
+
+module.exports = mongoose.model("JobPost", jobPostSchema);
