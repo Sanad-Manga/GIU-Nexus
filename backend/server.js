6f2453bf744b99bf15f2dfff3204101496c929d3
Ziad Mohsen <ziadmohsen782006@@gmail.com>
2026-04-20 20:44:43 +0200

    server file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-    
+    const mongoose = require("mongoose");
+
+// MongoDB connection string
+const MONGO_URI = "mongodb://localhost:27017/giu-nexus";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB successfully!");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+  });
