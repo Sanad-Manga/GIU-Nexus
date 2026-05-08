a24c8f1862f4b41b088b803fd037499e400ebe71
Sanad-Manga <ahmed.sanad@student.giu-uni.de>
2026-05-08 20:38:13 +0300

    s

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ const errorHandler = require("./middleware/ErrorHandler");
 
 const app = express();
 
-app.set('trust proxy', 1);
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.json());
